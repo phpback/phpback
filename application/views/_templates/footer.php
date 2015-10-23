97c45d0876bd55a9d07b2a743df89dcb67a3a622
Ivan Diaz <ivan@opensupports.com>
2015-10-22 21:31:18 -0300

    Fix issue #12 - Fix 'Delete Idea' button.

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,10 +1,6 @@
 	</div>
 	<hr>
 	<div style="text-align:center">  &copy; 2014 - Powered by <a href="http://www.phpback.org/" target="_blank">PHPBack</a></div>
-	<script>
-	$('.popover-with-html').popover({ html : true });
-	$('.contentdiv').css('width', '100%').css('width', '-=400px');
-   </script>
     <script src="<?php echo base_url(); ?>public/js/jquery-1.8.3.min.js"></script>
     <script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>public/js/bootstrap-select.js"></script>
@@ -15,5 +11,9 @@
     <script src="<?php echo base_url(); ?>public/js/jquery.placeholder.js"></script>
     <script src="<?php echo base_url(); ?>public/js/jquery.stacktable.js"></script>
     <script src="<?php echo base_url(); ?>public/bootstrap/js/application.js"></script>
+    <script>
+        $('.popover-with-html').popover({ html : true });
+        $('.contentdiv').css('width', '100%').css('width', '-=400px');
+    </script>
 </body>
 </html>
