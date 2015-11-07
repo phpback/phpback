79f689711cca92b0a8dd264a466084c038dd185e
Ivan Diaz <ivan@opensupports.com>
2015-11-07 12:17:03 -0300

    [TESTS] - Add initial installtion tests

--- tests/cases/InstallTest.php
+++ tests/cases/InstallTest.php
@@ -2,11 +2,76 @@
 require_once '_TestCase.php';
 
 class InstallTest extends TestCase {
-    public function testHasLoginForm() {
+    public function testDatabaseInstallation() {
         $this->url('index.php');
 
-        $username = $this->byName('hostname');
+        //All Fields initially with empty
+        $fields = $this->getFields(array(
+            'hostname',
+            'username',
+            'password',
+            'database',
+            'adminname',
+            'adminemail',
+            'adminpass',
+            'adminrpass'
+        ));
 
-        $this->assertEquals('', $username->value());
+        foreach ($fields as $field)  {
+            $this->assertEquals('', $field->value());
+        }
+
+        //Populate all fields
+        $fields['hostname']->value('localhost');
+        $fields['username']->value('root');
+        $fields['password']->value('');
+        $fields['database']->value('phpback_test');
+        $fields['adminemail']->value('admin@phpback.org');
+        $fields['adminname']->value('admin');
+        $fields['adminpass']->value('admin');
+        $fields['adminrpass']->value('admin');
+
+        //Submit form
+        $this->byName('install-form')->submit();
+
+        //Should delete first intallation files
+        $this->assertFileNotExists('install/index.php');
+        $this->assertFileNotExists('install/install1.php');
+        $this->assertFileNotExists('install/database_tables.sql');
+
+        //Should create configuration file
+        $this->assertFileExists('application/config/database.php');
+        include 'application/config/database.php';
+        $this->assertEquals($db['default']['username'], 'root');
+        $this->assertEquals($db['default']['password'], '');
+        $this->assertEquals($db['default']['database'], 'phpback_test');
+        $this->assertEquals($db['default']['username'], 'root');
+        $this->assertEquals($db['default']['dbdriver'], 'mysql');
     }
+
+    /**
+     * @depends testDatabaseInstallation
+     */
+     public function testConfigurationInstallation($value='') {
+         $this->url('install/index2.php');
+
+         $fields = $this->getFields(array(
+             'title',
+             'mainmail',
+         ));
+
+         //Populate all fields
+         $fields['title']->value('TestBack');
+         $fields['mainmail']->value('admin@phpback.org');
+
+         //Submit form
+         $this->byName('install2-form')->submit();
+
+         //Should delete first intallation files
+         $this->assertFileNotExists('install/index2.php');
+         $this->assertFileNotExists('install/install2.php');
+
+         //Should update database
+
+     }
 }
--- tests/cases/_TestCase.php
+++ tests/cases/_TestCase.php
@@ -2,10 +2,27 @@
 require_once 'tests/vendor/autoload.php';
 
 class TestCase extends PHPUnit_Extensions_Selenium2TestCase {
+    protected $mysql;
+
     public function setUp() {
         $this->setHost('localhost');
         $this->setPort(4444);
         $this->setBrowser('firefox');
         $this->setBrowserUrl('http://localhost:8080/');
+        $this->connectDatabase();
+    }
+
+    protected function getFields($array) {
+        $fields = array();
+
+        foreach ($array as $value) {
+            $fields[$value] = $this->byName($value);
+        }
+
+        return $fields;
+    }
+
+    private function connectDatabase() {
+
     }
 }
