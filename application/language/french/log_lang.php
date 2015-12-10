a9d34d032e32f652a2a7f67f2e1613daaa93c9ff
loic <lgevrey@oxya.com>
2015-12-10 12:48:18 +0100

    Begin translation

---
+++ application/language/french/log_lang.php
@@ -0,0 +1,18 @@
+<?php
+    $lang['log_commented'] = 'Idea %s commented';
+    $lang['log_comment_deleted'] = 'Deleted comment %s';
+    $lang['log_idea_approved'] = 'Approved idea %s';
+    $lang['log_idea_deleted'] = 'Deleted idea %s';
+    $lang['log_idea_status'] = 'Changed status of idea %s1 to %s2';
+    $lang['log_user_registered'] = 'User registered';
+    $lang['log_new_idea'] = 'New idea created';
+    $lang['log_idea_voted'] = 'Idea %s1 voted with %s2 votes';
+    $lang['log_user_banned'] = 'Banned user %s1 for %s2 days';
+    $lang['log_user_was_banned'] = 'User %s has been banned';
+    $lang['log_user_unbanned'] = 'Disabled ban for user %s';
+    $lang['log_settings'] = 'Settings have been edited';
+    $lang['log_user_admin'] = 'User has been updated to administrator';
+    $lang['log_category_created'] = 'category has been created';
+    $lang['log_category_description'] = 'description was updated';
+    $lang['log_category_changed'] = 'Category \'%s1\' changed to \'%s2\'';
+    $lang['log_category_deleted'] = 'Category %s deleted';
