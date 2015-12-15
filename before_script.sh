4a21ea0560095e5efbadcf08f3b1df72f8862a9b
Ivan Diaz <ivan@opensupports.com>
2015-12-14 22:08:56 -0300

    Testing - New travis approach

--- before_script.sh
+++ before_script.sh
@@ -3,30 +3,32 @@
 mysql -e 'create database phpback_test;'
 git checkout -- install
 rm application/config/database.php
+php -S localhost:8080
 
-SELENIUM_HUB_URL='http://127.0.0.1:4444'
-SELENIUM_JAR=/usr/share/selenium/selenium-server-standalone.jar
-SELENIUM_DOWNLOAD_URL=http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
-PHP_VERSION=$(php -v)
+serverUrl='http://127.0.0.1:4444'
+serverFile=selenium-server-standalone-2.35.0.jar
+firefoxUrl=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/21.0/linux-x86_64/en-US/firefox-21.0.tar.bz2
+firefoxFile=firefox.tar.bz2
+phpVersion=`php -v`
 
-ESCAPED_BUILD_DIR=$(echo "$TRAVIS_BUILD_DIR" | sed 's/\//\\\//g')
+pear channel-discover pear.phpunit.de
+pear install phpunit/PHP_Invoker
+pear install phpunit/DbUnit
+pear install phpunit/PHPUnit_Selenium
+pear install phpunit/PHPUnit_Story
 
-apt-get update
+echo "Download Firefox"
+wget $firefoxUrl -O $firefoxFile
+tar xvjf $firefoxFile
 
-echo "Installing Firefox"
-apt-get install firefox -y --no-install-recommends
-
-if [ ! -f "$SELENIUM_JAR" ]; then
-    echo "Downloading Selenium"
-    sudo mkdir -p $(dirname "$SELENIUM_JAR")
-    sudo wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
+echo "Starting xvfb"
+echo "Starting Selenium"
+if [ ! -f $serverFile ]; then
+    wget http://selenium.googlecode.com/files/$serverFile
 fi
+sudo xvfb-run java -jar $serverFile > /tmp/selenium.log &
 
-killall -9 java
-killall -9 Xvfb
-rm -f /tmp/.X99-lock
-
-wget --retry-connrefused --tries=120 --waitretry=3 --output-file=/dev/null "$SELENIUM_HUB_URL/wd/hub/status" -O /dev/null
+wget --retry-connrefused --tries=60 --waitretry=1 --output-file=/dev/null $serverUrl/wd/hub/status -O /dev/null
 if [ ! $? -eq 0 ]; then
     echo "Selenium Server not started"
 else
