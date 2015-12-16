3fecd2ea33925284f125a11d64f7da4523f33786
Ivan Diaz <ivan@opensupports.com>
2015-12-15 21:53:02 -0300

    Testing - Update port

--- before_script.sh
+++ before_script.sh
@@ -5,11 +5,14 @@ git checkout -- install
 rm application/config/database.php
 
 
-sudo apt-get install firefox
+SELENIUM_HUB_URL='http://127.0.0.1:4444'
+
+sudo apt-get install firefox -y --no-install-recommends
+echo "FIREFOX VERSION --------------------------"
 firefox --version
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar
 php -S localhost:8080 -t ./ &
-java -jar selenium-server-standalone-2.31.0.jar &
+wget --retry-connrefused --tries=120 --waitretry=3 --output-file=/dev/null "$SELENIUM_HUB_URL/wd/hub/status" -O /dev/null &
 sleep 30
