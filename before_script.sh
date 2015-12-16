9e2d364ca574aa229e94d8a851a90183aaf97e3b
Ivan Diaz <ivan@opensupports.com>
2015-12-15 21:21:56 -0300

    Testing - Update port

--- before_script.sh
+++ before_script.sh
@@ -5,7 +5,9 @@ git checkout -- install
 rm application/config/database.php
 
 
+sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar
-sh -e /etc/init.d/xvfb start
 php -S localhost:8080 -t ./ &
+java -jar selenium-server-standalone-2.15.0.jar &
+sleep 30
