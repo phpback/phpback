1131305374a5631b4315f769f80afa0329c8fa54
Ivan Diaz <ivan@opensupports.com>
2015-12-15 21:23:36 -0300

    Testing - Update port

--- before_script.sh
+++ before_script.sh
@@ -9,5 +9,5 @@ sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar
 php -S localhost:8080 -t ./ &
-java -jar selenium-server-standalone-2.15.0.jar &
+java -jar selenium-server-standalone-2.31.0.jar &
 sleep 30
