40e5d5a25eca8d2a3985d0ad0684a23e1968b197
Ivan Diaz <ivan@opensupports.com>
2015-12-21 20:14:26 -0300

    Update before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -4,10 +4,22 @@ mysql -e 'create database phpback_test;'
 git checkout -- install
 rm application/config/database.php
 
+sudo apt-get install supervisor -y --no-install-recommends
+sudo cp ./.ci/phpunit-environment.conf /etc/supervisor/conf.d/
+sudo sed -i "s/^directory=.*webserver$/directory=${ESCAPED_BUILD_DIR}\\/selenium-1-tests/" /etc/supervisor/conf.d/phpunit-environment.conf
+sudo sed -i "s/^autostart=.*selenium$/autostart=true/" /etc/supervisor/conf.d/phpunit-environment.conf
+sudo sed -i "s/^autostart=.*python-webserver$/autostart=true/" /etc/supervisor/conf.d/phpunit-environment.conf
+
+sudo killall supervisord
+sudo killall -9 java
+sudo killall -9 Xvfb
+sudo rm -f /tmp/.X99-lock
+sudo /etc/init.d/supervisor start
+
+
 sudo apt-get install -y --force-yes apache2 libapache2-mod-php5 php5-curl php5-mysql php5-intl
 sudo sed -i -e "s,/var/www,$(pwd)/web,g" /etc/apache2/sites-available/default
 sudo /etc/init.d/apache2 restart
-
 
 SELENIUM_HUB_URL='http://localhost:4444'
 
