39190c96500c5c4ca7b8f3c70c7b499504b9b76f
Ivan Diaz <ivan@opensupports.com>
2015-12-21 19:29:29 -0300

    Update before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -15,4 +15,5 @@ export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar
 php -S localhost:8080 -t ./ &
 wget --retry-connrefused --tries=120 --waitretry=3 --output-file=/dev/null "$SELENIUM_HUB_URL/wd/hub/status" -O /dev/null &
+java -jar selenium-server-standalone-2.31.0.jar &
 sleep 30
