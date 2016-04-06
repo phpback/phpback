b8f48802cfab0b7352d54239d5246e06587917f4
Ivan Diaz <ivan@opensupports.com>
2016-04-06 14:11:20 -0300

    Fix #52 - Idea should not be new to be counted

--- application/models/post.php
+++ application/models/post.php
@@ -223,7 +223,7 @@ class Post extends CI_Model
 
         $cat = $this->get_row_by_id('categories', $idea->categoryid);
 
-        if ($cat->status !== 'considered') {
+        if ($cat->status !== 'new') {
             $this->update_by_id('categories', 'ideas', $cat->ideas - 1, $cat->id);
         }
 
