8b494cafbbd04351477dac3a070447faf038d0ba
aixxo <okuhl@ukaachen.de>
2016-06-22 22:19:25 +0200

    german translation
    - created 'german' directory in languages
    - translated the files in german

---
+++ application/language/german/default_lang.php
@@ -0,0 +1,77 @@
+<?php
+    $lang['label_activity'] = 'Aktivität';
+    $lang['label_ban_user'] = 'Benutzer sperren';
+    $lang['label_categories'] = 'Kategorien';
+    $lang['label_category'] = 'Kategorie';
+    $lang['label_change_password'] = 'Kennwort ändern';
+    $lang['label_change_status'] = 'Change Status';
+    $lang['label_commented'] = 'Kommentiert in';
+    $lang['label_comments'] = 'Kommentare';
+    $lang['label_date'] = 'Datum';
+    $lang['label_delete_comment'] = 'Kommentar löschen';
+    $lang['label_delete_idea'] = 'Idee löschen';
+    $lang['label_delete_votes'] = 'Stimme löschen';
+    $lang['label_description'] = 'Beschreibung';
+    $lang['label_idea'] = 'Idee';
+    $lang['label_idea_title'] = 'Ideentitel';
+    $lang['label_ideas'] = 'Ideen';
+    $lang['label_log'] = 'Protokoll';
+    $lang['label_log_in'] = 'Anmelden';
+    $lang['label_log_out'] = 'Abmelden';
+    $lang['label_logged_as'] = 'Angemeldet als';
+    $lang['label_post_new_idea'] = 'Neue Idee';
+    $lang['label_profiles'] = 'Profile';
+    $lang['label_registration'] = 'Registrieren';
+    $lang['label_registration_form'] = 'Registrierungsformular';
+    $lang['label_reset_votes'] = 'Stimmen zurücksetzen';
+    $lang['label_search'] = 'Suchen';
+    $lang['label_sign_in'] = 'Anmelden';
+    $lang['label_submit'] = 'Übermitteln';
+    $lang['label_vote'] = 'Wählen';
+    $lang['label_votes'] = 'Stimmen';
+
+    $lang['text_shared_this_idea'] = 'Idee teilen';
+    $lang['text_flag_comment'] = 'Kommentar markieren';
+    $lang['text_create_an_account'] = 'Zugang erstellen';
+    $lang['text_registration_success'] = 'Registrierung erfolgreich';
+    $lang['text_select_category'] = 'Kategorie wählen...';
+    $lang['text_nothing_found'] = 'Kein Ergebniss...';
+    $lang['text_votes_left'] = 'verbleibende Stimmen.';
+    $lang['text_sure_delete_idea'] = 'Sind Sie sicher, das Sie diese Idee löschen wollen?';
+    $lang['text_sure_delete_comment'] = 'Sind Sie sicher, das Sie diesen Kommentar löschen wollen?';
+
+    $lang['idea_completed'] = 'Fertiggestellt';
+    $lang['idea_started'] = 'Begonnen';
+    $lang['idea_planned'] = 'Geplant';
+    $lang['idea_considered'] = 'In Betracht gezogen';
+    $lang['idea_declined'] = 'Abgelehnt';
+    $lang['idea_new'] = 'in Prüfung';
+
+    $lang['last_completed_ideas'] = 'Fertiggestellte Ideen';
+    $lang['last_started_ideas'] = 'Angefangene Ideen';
+    $lang['last_planned_ideas'] = 'Geplante Ideen';
+    $lang['last_considered_ideas'] = 'In Betracht gezogene Ideen';
+
+    $lang['form_remember'] = 'Erinnerung';
+    $lang['form_full_name'] = 'Vollständiger Name';
+    $lang['form_email'] = 'E-Mail Adresse';
+    $lang['form_password'] = 'Kennwort';
+    $lang['form_repeat_password'] = 'Kennwort wiederholen';
+    $lang['form_password_old'] = 'Altes Kennwort';
+    $lang['form_password_new'] = 'Neues Kennwort';
+    $lang['from_password_new_repeat'] = 'Neue sKennwort wiederholen';
+
+    $lang['error_login_to_post'] = 'Sie müssen sich anmelden, um eine neue Idee zu speichern';
+    $lang['error_login'] = 'Ungültiges Kennwort oder E-Mail Adresse';
+    $lang['error_recaptcha'] = 'Ungültiger Recaptcha Code';
+    $lang['error_name'] = 'Ungültiger Name';
+    $lang['error_email'] = 'Ungültige E-Mail';
+    $lang['error_password'] = 'Das Kennwort muss mindestens 6 Zeichen haben';
+    $lang['error_password_old'] = 'Altes Kennwort ist falsch';
+    $lang['error_passwords'] = 'Kennwörter stimmen nicht überein';
+    $lang['error_email_exists'] = 'E-Mail existiert schon';
+    $lang['error_banned'] = 'Sie wurden für %s Tage gesperrt';
+    $lang['error_banned_inf'] = 'Sie wurden für immer gesperrt!';
+    $lang['error_title'] = 'Titel ist zu kurz';
+    $lang['error_category'] = 'Wählen Sie eine Kategorie';
+    $lang['error_description'] = 'Beschreibung ist zu kurz';
