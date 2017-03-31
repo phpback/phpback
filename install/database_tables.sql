9b388e8104f93a73aed37789525d23d03c851e9a
DanielRuf <daniel@daniel-ruf.de>
2017-03-31 20:24:01 +0200

    add install directory

---
+++ install/database_tables.sql
@@ -0,0 +1,120 @@
+--
+-- This script builds the schema of PHPBack application
+--
+
+--
+-- Structure of table `categories`
+-- Categories of suggested ideas
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `ideas` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `comments`
+-- Comments are linked to ideas and submitted by an existing user
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `ideaid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `date` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `flags`
+--
+CREATE TABLE IF NOT EXISTS `flags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `toflagid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `date` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `ideas`
+--
+CREATE TABLE IF NOT EXISTS `ideas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `authorid` int(11) NOT NULL,
+  `date` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `votes` int(11) NOT NULL,
+  `comments` int(11) NOT NULL,
+  `status` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `categoryid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `logs`
+-- Application logs
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `date` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `type` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `toid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `settings`
+-- Key/Value pairs of settings
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `value` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `users`
+-- Users can have role admin
+-- Users can be banned from application
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `email` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `pass` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `votes` int(11) NOT NULL,
+  `isadmin` tinyint(1) NOT NULL,
+  `banned` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `votes`
+-- Votes are linked to ideas and submitted by an existing user
+CREATE TABLE IF NOT EXISTS `votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ideaid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure of table `_sessions`
+-- PHP sessions are persisted into database
+CREATE TABLE IF NOT EXISTS `_sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `token` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
