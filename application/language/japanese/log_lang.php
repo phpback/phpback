4d00d8dd311ae7187ac963668fef3c1c5278a8d6
WhiteRaven777 <WhiteRaven777@users.noreply.github.com>
2017-11-09 17:07:27 +0900

    localise - Japanese
    Localization files for Japanese has been added.

---
+++ application/language/japanese/log_lang.php
@@ -0,0 +1,18 @@
+<?php
+    $lang['log_commented'] = 'アイデア %s にコメント';
+    $lang['log_comment_deleted'] = 'コメント %s を削除';
+    $lang['log_idea_approved'] = 'アイデア %s を承認';
+    $lang['log_idea_deleted'] = 'アイデア %s を削除';
+    $lang['log_idea_status'] = 'アイデア %s1 のステータスを %s2 に変更';
+    $lang['log_user_registered'] = 'ユーザーを登録';
+    $lang['log_new_idea'] = '新しいアイデアを作成';
+    $lang['log_idea_voted'] = 'アイデア %s1 に %s2 票投じられました';
+    $lang['log_user_banned'] = 'ユーザー「%s1」を %s2 日間BANします';
+    $lang['log_user_was_banned'] = 'ユーザー「%s」はBANされています';
+    $lang['log_user_unbanned'] = 'ユーザー「%s」のBAN状態を解除しました';
+    $lang['log_settings'] = '設定が変更されました';
+    $lang['log_user_admin'] = 'ユーザーが管理者に更新されました';
+    $lang['log_category_created'] = 'カテゴリが作成されました';
+    $lang['log_category_description'] = '説明が更新されました';
+    $lang['log_category_changed'] = 'カテゴリが「%s1」から「%s2」に変更されました';
+    $lang['log_category_deleted'] = '%s カテゴリを削除';
